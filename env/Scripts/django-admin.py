5ab30f4fd28ba980120facf3d3ca18034750b834
oscar_al8 <oscarmehrer@hotmail.com>
2021-07-21 12:55:57 -0400

    Initialize repo

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\oscar\desktop\udemy - build python django real project django web development 2020-9\carzone-project\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
